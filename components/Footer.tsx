2c914e4f196faaf285f2e57de3e4e5cd9eaf2a14
Ali Mohamed <110411510+alim-99@users.noreply.github.com>
2025-02-04 16:30:22 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
 
       <div className='flex mt-16 md:flex-row flex-col 
       justify-between items-center'>
-        <p className='md:text-base text-sm md:font-normal font-light mt-5'>Copyright © 2024 Ali</p>
+        <p className='md:text-base text-sm md:font-normal font-light mt-5'>Copyright © {new Date().getFullYear()} Ali</p>
 
         <div className='flex items-center md:gap-3 gap-6'>
           {socialMedia.map((profile) => ( 
